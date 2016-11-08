5ab111ec56681b7863f43d66f76e4180e0da1a7e
Hailey WU <hailey@dhcp-128-189-67-173.ubcsecure.wireless.ubc.ca>
2016-11-07 18:49:28 -0800

    change phone number to string, add purchase quantity to productOrder, add few functions in Customer, SephoraMember, and PurchaseHistory to make the purchase and return Product, Product class all functions fulfilled besdies the nested one

--- src/VIBClass/Employee.java
+++ src/VIBClass/Employee.java
@@ -48,7 +48,7 @@ public class Employee {
 
     //return the list of name of customer who has birthday on the current date
     public ArrayList<String> birthdayGift(Date currentDate) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList();
         ResultSet rs = null;
         String query = "SELECT name FROM customer WHERE birthday = " + currentDate;
         rs = oraManager.query(query);
@@ -67,7 +67,7 @@ public class Employee {
 
     // select all tuples with inventory less than low
     public ArrayList<Product> lowStockReport(int low) {
-        ArrayList<Product> list = new ArrayList<>();
+        ArrayList<Product> list = new ArrayList<Product>();
         ResultSet rs = null;
         String query = "SELECT * FROM product WHERE inventoryNumber <= " + low;
         rs = oraManager.query(query);
@@ -91,7 +91,7 @@ public class Employee {
 
     // select all tuples in product that has a price higher than the given price
     public ArrayList<Product> HigherPriceReport(int givenprice) {
-        ArrayList<Product> list = new ArrayList<>();
+        ArrayList<Product> list = new ArrayList<Product>();
         ResultSet rs = null;
         String query = "SELECT * FROM product WHERE price >= " + givenprice;
         rs = oraManager.query(query);
@@ -115,7 +115,7 @@ public class Employee {
 
     // select all tuples in product that has a price higher than the given price
     public ArrayList<Product> LowerPriceReport(int givenprice) {
-        ArrayList<Product> list = new ArrayList<>();
+        ArrayList<Product> list = new ArrayList<Product>();
         ResultSet rs = null;
         String query = "SELECT * FROM product WHERE price <= " + givenprice;
         rs = oraManager.query(query);
