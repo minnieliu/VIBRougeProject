e98ce719e73636418ab929cbf6c6ab21d5ff72e3
ivyluo96 <ivyluo_2011@hotmail.com>
2016-11-13 19:39:17 -0800

    Added more tuples

--- src/resources/VIBRouge.sql
+++ src/resources/VIBRouge.sql
@@ -1,6 +1,5 @@
 drop table employee;
 drop table service;
-drop table updateStatus;
 drop table member1;
 drop table purchaseOrder;
 drop table customer;
@@ -26,7 +25,7 @@ CREATE TABLE member1
   password        CHAR(30),
   birthday        CHAR(20),
   currentPoints   INTEGER,
-  currentStatus   CHAR(20) DEFAULT 'Beauty Insider',
+  currentStatus   CHAR(20),
   name            CHAR(20) NOT NULL,
   phoneNumber     CHAR(20) NOT NULL,
   CONSTRAINT member1_PK PRIMARY KEY (accountNo),
@@ -103,10 +102,43 @@ insert into customer
 insert into customer
     values('Sarah Kwong','7782341039','F');
 
+insert into customer
+    values('Bob Lee','6042394856','M');
+
+insert into customer
+    values('Christine Liang','7784953432','F');
+
+insert into customer
+    values('Lily Smith','6047563847','F');
+
+insert into customer
+    values('Elizabeth Kwong','7786453746','F');
+
+insert into customer
+    values('Sophia Smith','7786453647','F');
+
+insert into customer
+    values('Elizabeth Martin','6049577765','F');
+
 
 
 insert into member1
-    values(42590000,140,'sarah_kwong@yahoo.com','eciwhe1','1988-09-24', 280, 'BeautyInsider', 'Sarah Kwong','7782341039');
+    values(83746566,58,'soph_smith@hotmail.com','1111111','1972-09-24', 900, 'Beauty Insider', 'Sophia Smith','7786453647');
+
+insert into member1
+    values(84756475,5000,'eliz_kwong@hotmail.com','1i2837jf','1992-09-18', 38475, 'VIB Rouge', 'Elizabeth Kwong','7786453746');
+
+insert into member1
+    values(12837465,300,'lily_smith@gmail.com','1mmmdfea','1996-09-13', 3874, 'VIB', 'Lily Smith','6047563847');
+
+insert into member1
+    values(12763746,4885,'bob_lee@gmail.com','128jdh&','1998-09-28', 3500, 'VIB Rouge', 'Bob Lee','6042394856');
+
+insert into member1
+    values(12837485,10,'chrissy_liang@gmail.com','1j7fj**','1978-09-30', 200, 'Beauty Insider', 'Christine Liang','7784953432');
+
+insert into member1
+    values(42590000,140,'sarah_kwong@yahoo.com','eciwhe1','1988-09-24', 280, 'Beauty Insider', 'Sarah Kwong','7782341039');
 
 insert into member1
     values(39512350,400,'jpeter@gmail.com','12kdj8*','1980-02-02', 2500 , 'VIB','Jessica Peters','6042958190');
@@ -124,7 +156,7 @@ insert into employee
     values(13579,'John Smith');
 
 insert into employee
-    values(12345,'Elizabeth Martin');
+    values(12345,'Elizabeth Martine');
 
 insert into employee
     values(12645,'Adam Smith');
@@ -137,6 +169,25 @@ insert into employee
 
 insert into employee
     values(36475,'Carter Wong');
+
+insert into employee
+    values(38847,'Ronny Stevens');
+
+insert into employee
+    values(73846,'Tiffany Lee');
+
+insert into employee
+    values(37485,'Timmy Lerr');
+
+insert into employee
+    values(73847,'Rihanna Depp');
+
+insert into employee
+    values(64536,'Arianna Lee');
+
+insert into employee
+    values(18273,'Leigh Smeenths');
+
 
 insert into service
     values(123456,'2016-2-15',20,'make-up tutorial');
@@ -155,6 +206,23 @@ insert into service
 
 insert into service
     values(765432,'2016-8-24',20, 'meet the artist');
+
+insert into service
+    values(374657,'2016-11-01',30, 'Start of month special');
+
+insert into service
+    values(172635,'2016-11-02',35, '2nd day of November');
+
+insert into service
+    values(273645,'2016-11-03',15, 'Meet the employees');
+
+insert into service
+    values(277734,'2016-11-04',25, 'Meet the producers');
+
+insert into service
+    values(374656,'2016-11-05',20, '5th day of month special');
+
+
 
 insert into product
     values(5555,64.99,'Becca',53, 'makeup');
@@ -177,10 +245,68 @@ insert into product
 insert into product
     values(3333,41.99,'Chloe',24, 'fragrance');
 
+insert into product
+    values(1485,1000.99,'Expensif',4, 'nail');
+
+insert into product
+    values(9957,1.99,'cherCheap',1000, 'skinCare');
+
+insert into product
+    values(2773,1000000,'MostExpensif',3, 'fragrance');
+
+insert into product
+    values(4475,2.99,'Cheep',4, 'pointPerks');
+
+insert into product
+    values(3384,11.99,'cherCheap',40, 'skinCare');
+
+insert into product
+    values(4950,100.99,'Expensif',40000, 'nail');
+
+insert into productOrder
+    values(29993846,5555,1);
+insert into purchaseOrder
+    values(29993846,'6049577765', 'Elizabeth Martin', 'visa','2016-10-17');
+
+insert into productOrder
+    values(22888837,5555,10);
+insert into purchaseOrder
+    values(22888837,'7786453647', 'Sophia Smith', 'visa','2016-11-21');
+
+insert into productOrder
+    values(33388847,5555,1);
+insert into purchaseOrder
+    values(33388847,'7786453746', 'Elizabeth Kwong', 'visa','2016-11-25');
+
+insert into productOrder
+    values(99847756,5555,1);
+insert into purchaseOrder
+    values(99847756,'6047563847', 'Lily Smith', 'debit','2016-08-25');
+
+insert into productOrder
+    values(37485757,5555,1);
+insert into productOrder
+    values(38488857,2773,1);
+insert into productOrder
+    values(22736445,9957,3);
+insert into purchaseOrder
+    values(37485757,'7784953432', 'Christine Liang', 'debit','2016-03-25');
+insert into purchaseOrder
+    values(38488857,'7784953432', 'Christine Liang', 'credit','2016-03-29');
+insert into purchaseOrder
+    values(22736445,'7784953432', 'Christine Liang', 'debit','2016-04-25');
+
+insert into productOrder
+    values(12333333,4950,3);
+insert into productOrder
+    values(12344444,5555,1);
+insert into purchaseOrder
+    values(12344444,'6042394856', 'Bob Lee', 'cash','2016-07-23');
+insert into purchaseOrder
+    values(12333333,'6042394856', 'Bob Lee', 'cash','2016-07-23');
 
 insert into productOrder
     values(68019349,5555,1);
-
 insert into productOrder
     values(68019349,3333,1);
 insert into productOrder
@@ -193,6 +319,7 @@ insert into productOrder
     values(68019350,1000,1);
 insert into productOrder
     values(68019349,5151,1);
+
 
 insert into purchaseOrder
     values(68019349,'7789849871', 'Elaine Wong', 'credit','2016-07-07');
@@ -288,3 +415,4 @@ BEGIN
     DELETE FROM productOrder WHERE productID = :OLD.productID;
 END;
 /
+
